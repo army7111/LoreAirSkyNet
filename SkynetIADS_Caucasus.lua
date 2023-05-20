de0a75cd20df9bd2cc8a88ecfb81d82814a3d41f
army711 <army711live@gmail.com>
2023-05-20 17:21:09 +0200

    v6

--- SkynetIADS_Caucasus.lua
+++ SkynetIADS_Caucasus.lua
@@ -1,27 +1,42 @@
 --- Crea Sistema IADS RED
-redIADS = SkynetIADS:create('RED IADS')
+RedIADS = SkynetIADS:create('RED IADS')
 
 local commandCenter = StaticObject.getByName("Command Center")
-redIADS:addCommandCenter(commandCenter)
+RedIADS:addCommandCenter(commandCenter)
 --- Aggiunge tutte le unità che hanno come prefisso REDSAM - REDEW - REDCC
-redIADS:addSAMSitesByPrefix('REDSAM')
-redIADS:addEarlyWarningRadarsByPrefix('REDEW')
-redIADS:addRadioMenu()
-redIADS:setUpdateInterval(5)
-redIADS:setGoLiveRangeInPercent(100)
-redIADS:GO_LIVE_WHEN_IN_SEARCH_RANGE()
+RedIADS:addSAMSitesByPrefix('REDSAM')
+RedIADS:addEarlyWarningRadarsByPrefix('REDEW')
+RedIADS:addRadioMenu()
+RedIADS:setUpdateInterval(5)
+RedIADS:getSAMSiteByGroupName('REDSAM-S300'):setActAsEW(true)
+
+-- Impostazione del range di ingaggio al massimo per tutti i siti SAM
+local allSAMSites = RedIADS:getSAMSites()
+for i, samSite in ipairs(allSAMSites) do
+   -- samSite:setHARMDetectionChance(100)
+    samSite:setEngagementZone(SkynetIADSAbstractRadarElement.GO_LIVE_WHEN_IN_SEARCH_RANGE)
+end
+
+
+
+-- Dichiarazione Point Defence 
+local sa15PDKTS = RedIADS:getSAMSiteByGroupName('REDSAM-PDKUTAISI')
+RedIADS:getSAMSiteByGroupName('REDSAM-S300'):addPointDefence(sa15PDKTS)
+
 
 -- Attiva sistema IADS Skynet
-redIADS:activate()
+RedIADS:activate()
 
 --- Debug ---
 
-local iadsDebug = redIADS:getDebugSettings()  
+local iadsDebug = RedIADS:getDebugSettings()  
 
 iadsDebug.IADSStatus = true
 iadsDebug.contacts = true
 iadsDebug.jammerProbability = true
 
+--iadsDebug.samSiteStatusEnvOutput = true
+--iadsDebug.earlyWarningRadarStatusEnvOutput = true
 iadsDebug.addedEWRadar = true
 iadsDebug.addedSAMSite = true
 iadsDebug.warnings = true
