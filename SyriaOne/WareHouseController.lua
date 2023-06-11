1059fb78a8c2241113743bc23f4fe8dfc9b1415d
army711 <army711live@gmail.com>
2023-06-11 17:39:00 +0200

    test

--- SyriaOne/WareHouseController.lua
+++ SyriaOne/WareHouseController.lua
@@ -5,28 +5,43 @@ local warehouses = {
     WAREHOUSE:New(STATIC:FindByName("WHCyproErcan"), "WHCyproErcan"),
 }
 
-warehouses.WHCyproEast:SetAutoDefenceOn(true)
-warehouses.WHCyproGeck:SetAutoDefenceOn(true)
-warehouses.WHCyproErcan:SetAutoDefenceOn(true)
+for i, warehouse in ipairs(warehouses) do
+    warehouse:SetAutoDefenceOn(true)
+    -- Dichiara tipo di Unità dai template
+    local tank=GROUP:FindByName("REDWARTank")
+    local infantry=GROUP:FindByName("REDWARInfantry")
+    local truck=GROUP:FindByName("REDWARTruck")
+    local sam=GROUP:FindByName("REDWARSA19")
+    local aaa=GROUP:FindByName("REDWARAAAShilka")
+    local rpg=GROUP:FindByName("REDWARInRPG")
+    -- Caricamento Asset Standard
+    warehouse:AddAsset(infantry, 20, WAREHOUSE.Attribute.GROUND_INFANTRY)
+    warehouse:AddAsset(tank, 8, WAREHOUSE.Attribute.GROUND_TANK)
+    warehouse:AddAsset(truck, 16, WAREHOUSE.Attribute.GROUND_TRUCK)
+    warehouse:AddAsset(sam, 8, WAREHOUSE.Attribute.GROUND_SAM)
+    warehouse:AddAsset(aaa, 8, WAREHOUSE.Attribute.GROUND_AAA)
+    warehouse:AddAsset(rpg, 20, WAREHOUSE.Attribute.GROUND_INFANTRY)
+end
 
+warehouses.WHCyproEast:AddRequest(warehouses.WHCyproGeck, WAREHOUSE.Descriptor.UNITTYPE, sam, 2)
 -- Dichiara tipo di Unità dai template
 
-local tank=GROUP:FindByName("REDWARTank")
-local infantry=GROUP:FindByName("REDWARInfantry")
-local truck=GROUP:FindByName("REDWARTruck")
-local sam=GROUP:FindByName("REDWARSA19")
-local aaa=GROUP:FindByName("REDWARAAAShilka")
-local rpg=GROUP:FindByName("REDWARInRPG")
+--local tank=GROUP:FindByName("REDWARTank")
+--local infantry=GROUP:FindByName("REDWARInfantry")
+--local truck=GROUP:FindByName("REDWARTruck")
+--local sam=GROUP:FindByName("REDWARSA19")
+--local aaa=GROUP:FindByName("REDWARAAAShilka")
+--local rpg=GROUP:FindByName("REDWARInRPG")
 
 -- Caricamento Asset Standard
 
-for _, warehouse in pairs(warehouses) do 
-    warehouse:AddAsset(infantry, 20)
-    warehouse:AddAsset(tank, 8)
-    warehouse:AddAsset(truck, 16)
-    warehouse:AddAsset(sam, 8)
-    warehouse:AddAsset(aaa, 8)
-    warehouse:AddAsset(rpg, 20)
-    warehouse:Start()
-end
+--for _, warehouse in pairs(warehouses) do 
+--    warehouse:AddAsset(infantry, 20)
+--    warehouse:AddAsset(tank, 8)
+--    warehouse:AddAsset(truck, 16)
+--    warehouse:AddAsset(sam, 8)
+--    warehouse:AddAsset(aaa, 8)
+--    warehouse:AddAsset(rpg, 20)
+--    warehouse:Start()
+--end
 
