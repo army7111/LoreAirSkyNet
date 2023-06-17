f0ddcc2835f4c1e4ed26119ca07c6549a0515f86
army711 <army711live@gmail.com>
2023-06-17 10:54:34 +0200

    edit

--- SyriaOne/SyriaOne_CSAR.lua
+++ SyriaOne/SyriaOne_CSAR.lua
@@ -1,4 +1,4 @@
-local syriaCSAR = CSAR:New(coalition.side.BLUE,"CSARPilot","SOS-Beacon")
+local syriaCSAR = CSAR:New(coalition.side.BLUE, "CSARPilot", "SOS-Beacon")
 syriaCSAR.immortalcrew = true
 syriaCSAR.invisiblecrew = true
 syriaCSAR.allowbronco = true
@@ -7,7 +7,8 @@ syriaCSAR.useprefix = true
 syriaCSAR.csarPrefix = "CSAR"
 
 function syriaCSAR:OnAfterPilotDown(From, Event, To, SpawnedGroup, Frequency, Leadername, CoordinatesText)
-    MESSAGE:New(string.format("Il pilota %s è abbattuto! La frequenza per il CSAR è %s KHz, le coordinate sono %s.", Leadername, Frequency, CoordinatesText), 15):ToAll()
+    MESSAGE:New(string.format("Il pilota %s è abbattuto! La frequenza per il CSAR è %s KHz, le coordinate sono %s.",
+        Leadername, Frequency, CoordinatesText), 15):ToAll()
 end
 
 syriaCSAR:Start()
@@ -19,28 +20,30 @@ local csarGroups = SET_GROUP:New():FilterPrefixes({"CSAR"}):FilterStart()
 local csarDetection = DETECTION_UNITS:New(csarGroups, csarZone, 1000)
 
 local function startCsarMission()
-  syriaCSAR:SpawnCSARAtZone(csarZone, coalition.side.BLUE, "PilotaTEST", true)
+    syriaCSAR:SpawnCSARAtZone(csarZone, coalition.side.BLUE, "PilotaTEST", true)
 end
 
 local csarMissionScheduler = SCHEDULER:New(nil, startCsarMission, {}, 0, 600)
 
-csarDetection:Start() 
+csarDetection:Start()
 
 CSAR_SCHEDULER_RUNNING = false
 
 function csarDetection:OnAfterDetect(EventData)
-  local detectedSets = EventData.DetectedSets
-  local firstSet = detectedSets and detectedSets[1]
-  
-  if firstSet and firstSet:Count() > 0 then
-    if not CSAR_SCHEDULER_RUNNING then
-        csarMissionScheduler:Start()
-        CSAR_SCHEDULER_RUNNING = true
+    local detectedSets = EventData.DetectedSets
+    local firstSet = detectedSets and detectedSets[1]
+
+    if firstSet and firstSet:Count() > 0 then
+        if not CSAR_SCHEDULER_RUNNING then
+            csarMissionScheduler:Start()
+            MESSAGE:New("Scheduler CSAR Avviato.", 15)
+            CSAR_SCHEDULER_RUNNING = true
+        end
+    else
+        if CSAR_SCHEDULER_RUNNING then
+            csarMissionScheduler:Stop()
+            MESSAGE:New("Scheduler CSAR Interrotto.", 15)
+            CSAR_SCHEDULER_RUNNING = false
+        end
     end
-  else
-    if CSAR_SCHEDULER_RUNNING then
-        csarMissionScheduler:Stop()
-        CSAR_SCHEDULER_RUNNING = false
-    end
-  end
 end
