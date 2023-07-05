12ec14a855a707c89ff9cc90f8e9b572bd31a3e2
army711 <army711live@gmail.com>
2023-07-05 18:13:16 +0200

    debug

--- SyriaOne/SyriaOneZoneCapture.lua
+++ SyriaOne/SyriaOneZoneCapture.lua
@@ -1,8 +1,12 @@
+-- Dichiarazione HQ
+US_CC = COMMANDCENTER:New(GROUP:FindByName("BLUEHQ"),"Blue Command Center")
+RU_CC = COMMANDCENTER:New(GROUP:FindByName("REDHQ"), "Red Command Center")
+
 -- Funzione per creare una nuova zona di cattura
 function newCaptureZone(t, name)
     local captureZone = ZONE:New("AZ" .. name)
     local zoneCaptureCoalition = ZONE_CAPTURE_COALITION:New(captureZone, coalition.side.RED)
-    zoneCaptureCoalition:__Guard(1) -- Imposta la zona nello stato di "Guarding"
+    zoneCaptureCoalition:__Guard(1) -- Imposta la zona nello stato di "Guarded"
     zoneCaptureCoalition:Start() -- Avvia il monitoraggio della zona
     t[name] = zoneCaptureCoalition
 end
@@ -23,20 +27,21 @@ newCaptureZone(captureZones, "EastCypro")
 newCaptureZone(captureZones, "Testadiponte")
 
 -- Funzione per gestire gli eventi di transizione delle zone di cattura
-function handleCaptureZoneEvent(self, From, Event, To)
-    local Coalition = self:GetCoalition()
-    self:E({ Coalition = Coalition })
 
-    -- Gestisci l'evento di cambio di stato
-    if From ~= To then
-        if Coalition == coalition.side.BLUE then
-            self:Smoke(SMOKECOLOR.Blue)
-            US_CC:MessageTypeToCoalition(string.format("%s is under protection of the USA", self:GetZoneName()), MESSAGE.Type.Information)
-            RU_CC:MessageTypeToCoalition(string.format("%s is under protection of the USA", self:GetZoneName()), MESSAGE.Type.Information)
-        else
-            self:Smoke(SMOKECOLOR.Red)
-            RU_CC:MessageTypeToCoalition(string.format("%s is under protection of Russia", self:GetZoneName()), MESSAGE.Type.Information)
-            US_CC:MessageTypeToCoalition(string.format("%s is under protection of Russia", self:GetZoneName()), MESSAGE.Type.Information)
-        end
-    end
-end
+-- function handleCaptureZoneEvent(self, From, Event, To)
+--     local Coalition = self:GetCoalition()
+--     self:E({ Coalition = Coalition })
+
+--     -- Gestisci l'evento di cambio di stato
+--     if From ~= To then
+--         if Coalition == coalition.side.BLUE then
+--             self:Smoke(SMOKECOLOR.Blue)
+--             US_CC:MessageTypeToCoalition(string.format("%s is under protection of the USA", self:GetZoneName()), MESSAGE.Type.Information)
+--             RU_CC:MessageTypeToCoalition(string.format("%s is under protection of the USA", self:GetZoneName()), MESSAGE.Type.Information)
+--         else
+--             self:Smoke(SMOKECOLOR.Red)
+--             RU_CC:MessageTypeToCoalition(string.format("%s is under protection of Russia", self:GetZoneName()), MESSAGE.Type.Information)
+--             US_CC:MessageTypeToCoalition(string.format("%s is under protection of Russia", self:GetZoneName()), MESSAGE.Type.Information)
+--         end
+--     end
+-- end
--- SyriaOne/WareHouseController.lua
+++ SyriaOne/WareHouseController.lua
@@ -5,7 +5,7 @@ WarehousesCypro = {}
 WarehousesCypro.Paphos = WAREHOUSE:New(STATIC:FindByName("WHPAPHOS"), "Warehouse Paphos")
 WarehousesCypro.Paphos:Start()
 -- Akrotiri
-WarehousesCypro.Akrotiri = WAREHOUSE:New(STATIC:FindByName("WHAKROTIRI"), "Warehouse Akroptiri")
+WarehousesCypro.Akrotiri = WAREHOUSE:New(STATIC:FindByName("WHAKROTIRI"), "Warehouse Akrotiri")
 WarehousesCypro.Akrotiri:Start()
 -- Pinarbashi
 WarehousesCypro.Pinarbashi = WAREHOUSE:New(STATIC:FindByName("WHPINARBASHI"), "Warehouse Pinarbashi")
@@ -45,13 +45,13 @@ WarehouseTarawa = WAREHOUSE:New(STATIC:FindByName("WHTarawa"), "Warehouse Tarawa
 WarehouseTarawa:Start()
 
 -- Inizializzazione Warehouse con unità
-WarehousesCypro.Ercan:AddAsset("Mi24", 50)
-WarehousesCypro.Ercan:AddAsset("Ka503", 50)
-WarehousesCypro.Paphos:AddAsset("TEMPL-RedTank", 50)
-WarehousesCypro.Paphos:AddAsset("TEMPL-SA15", 50)
+WarehousesCypro.Lakatamia:AddAsset("TEMPL-Mi24", 20)
+WarehousesCypro.Lakatamia:AddAsset("Ka503", 20)
+WarehousesCypro.Paphos:AddAsset("TEMPL-RedTank", 20)
+WarehousesCypro.Paphos:AddAsset("TEMPL-SA15", 10)
 WarehousesCypro.Paphos:AddAsset("TEMPL-RedTruck", 50)
 WarehousesCypro.Ercan:AddAsset("TEMPL-RedInf", 100)
-WarehousesCypro.Paphos:AddAsset("TEMPL-AirTransportRED", 50)
+WarehousesCypro.Paphos:AddAsset("TEMPL-AirTransportRED", 10)
 WarehousesCypro.Ercan:AddAsset("REDAICAP", 25)
 
 -- Dichiarazione Unita e tipo di unita
@@ -70,8 +70,12 @@ function RequestResource(fromWarehouse, toWarehouse, groupName, number)
     fromWarehouse:AddRequest(toWarehouse, WAREHOUSE.Descriptor.GROUPNAME, groupName, number)
 end
 
-function RequestResourceSelfProp(fromWarehouse, toWarehouse, groupName, number, TransportType)
-    fromWarehouse:AddRequest(toWarehouse, WAREHOUSE.Descriptor.GROUPNAME, groupName, number, SELFPROPELLED)
+function RequestResourceSelfProp(fromWarehouse, toWarehouse, groupName, number)
+    fromWarehouse:AddRequest(toWarehouse, WAREHOUSE.Descriptor.GROUPNAME, groupName, number, WAREHOUSE.TransportType.SELFPROPELLED)
+end
+
+function RequestResourceAirTrans(fromWarehouse, toWarehouse, groupName, number)
+    fromWarehouse:AddRequest(toWarehouse, WAREHOUSE.Descriptor.GROUPNAME, groupName, number, WAREHOUSE.TransportType.AIRPLANE)
 end
 --Coppie fromToWarehouse per funzione random di convogli
 
@@ -89,29 +93,67 @@ end
 FromToWarehouseCAP = {
     {WarehousesCypro.Ercan, WarehousesCypro.Akrotiri},
     {WarehousesCypro.Ercan, WarehousesCypro.Gecitkale},
-    {WarehousesCypro.Ercan, WarehousesCypro.Kingsfield},
     {WarehousesCypro.Ercan, WarehousesCypro.Larnaca},
     {WarehousesCypro.Ercan, WarehousesCypro.Paphos},
     {WarehousesCypro.Ercan, WarehousesCypro.Pinarbashi}
 }
 
+AirframesCAP = {"REDAICAP", "REDAICAP"}
+
+function ScheduleCAPRequest()
+    local warehousePair = FromToWarehouseCAP[math.random(#FromToWarehouseCAP)]
+    local currFromWarehouse = warehousePair[1]
+    local currToWarehouse = warehousePair[2]
+    local currGroup = AirframesCAP[math.random(#AirframesCAP)]
+    local currNumber = math.random(2)
+    RequestResource(currFromWarehouse, currToWarehouse, currGroup, currNumber)
+end
+
+-- ----------------------------------------------------------------------------------------------------------------
+
 FromToWarehouseGROUND = {
     {WarehousesCypro.Paphos, WarehousesCypro.Akrotiri},
-    --{WarehousesCypro.Paphos, WarehousesCypro.Gecitkale},
-    --{WarehousesCypro.Paphos, WarehousesCypro.Kingsfield},
     {WarehousesCypro.Paphos, WarehousesCypro.Larnaca}
-    --{WarehousesCypro.Paphos, WarehousesCypro.Ercan},
-    --{WarehousesCypro.Paphos, WarehousesCypro.Pinarbashi}
 }
 
+GroundTank = {"TEMPL-RedTank", "TEMPL-RedAAA","TEMPL-RedTruck"}
+
+function ScheduleGROUNDWHRefill()
+    local warehousePair = FromToWarehouseGROUND[math.random(#FromToWarehouseGROUND)]
+    local currFromWarehouse = warehousePair[1]
+    local currToWarehouse = warehousePair[2]
+    local currGroup = GroundTank[math.random(#GroundTank)]
+    local currNumber = math.random(2,2)
+    RequestResourceAirTrans(currFromWarehouse, currToWarehouse, currGroup, currNumber)
+end
+
+-- ----------------------------------------------------------------------------------------------------------------
+
 FromToWarehouseHeli = {
-    {WarehousesCypro.Ercan, WarehousesCypro.EastCypro},
-    {WarehousesCypro.Ercan, WarehousesCypro.Akrotiri},
-    {WarehousesCypro.Ercan, WarehousesCypro.Gecitkale},
-    {WarehousesCypro.Ercan, WarehousesCypro.Kingsfield},
-    {WarehousesCypro.Ercan, WarehousesCypro.Larnaca},
-    {WarehousesCypro.Ercan, WarehousesCypro.Pinarbashi}
+    {WarehousesCypro.Lakatamia, WarehousesCypro.EastCypro},
+    {WarehousesCypro.Lakatamia, WarehousesCypro.Gecitkale}
 }
+--{
+--    {WarehousesCypro.Lakatamia, WarehousesCypro.EastCypro}
+    --{WarehousesCypro.Lakatamia, WarehousesCypro.Akrotiri},
+    --{WarehousesCypro.Lakatamia, WarehousesCypro.Gecitkale},
+    --{WarehousesCypro.Lakatamia, WarehousesCypro.Kingsfield},
+    --{WarehousesCypro.Lakatamia, WarehousesCypro.Larnaca},
+    --{WarehousesCypro.Lakatamia, WarehousesCypro.Pinarbashi}
+--}
+
+AirframesHELI = {"Ka503", "TEMPL-Mi24"}
+
+function ScheduleHelirefill()
+    local warehousePair = FromToWarehouseHeli[math.random(#FromToWarehouseHeli)]
+    local currFromWarehouse = warehousePair[1]
+    local currToWarehouse = warehousePair[2]
+    local currGroup = AirframesHELI[math.random(#AirframesHELI)]
+    local currNumber = math.random(2)
+    RequestResourceSelfProp(currFromWarehouse, currToWarehouse, currGroup, currNumber)
+end
+
+-- ----------------------------------------------------------------------------------------------------------------
 
 FromToGroundPropelled = {
     {WarehousesCypro.Larnaca, WarehousesCypro.Testadiponte},
@@ -120,47 +162,28 @@ FromToGroundPropelled = {
     {WarehousesCypro.Akrotiri, WarehousesCypro.EastCypro}
 }
 
-AirframesCAP = {"REDAICAP", "REDAICAP"}
-AirframesHELI = {"Ka503", "TEMPL-Mi24"}
-GroundTank = {"TEMPL-RedTank", "TEMPL-RedAAA", "TEMPL-SA15"}
-
-function ScheduleCAPRequest()
-    local warehousePair = FromToWarehouseCAP[math.random(2, table.getn(FromToWarehouseCAP))]
-    local currFromWarehouse = warehousePair[1]
-    local currToWarehouse = warehousePair[2]
-    local currGroup = AirframesCAP[math.random(2, table.getn(AirframesCAP))]
-    --local currNumber = math.random(2,2)
-    RequestResource(currFromWarehouse, currToWarehouse, currGroup, 2)
-end
-
-function ScheduleWHrefill()
-    local warehousePair = FromToWarehouseHeli[math.random(2, table.getn(FromToWarehouseHeli))]
-    local currFromWarehouse = warehousePair[1]
-    local currToWarehouse = warehousePair[2]
-    local currGroup = AirframesHELI[math.random(2, table.getn(AirframesHELI))]
-    local currNumber = math.random(1,2)
-    RequestResource(currFromWarehouse, currToWarehouse, currGroup, currNumber)
-end
-
-function ScheduleGROUNDWHRefill()
-    local warehousePair = FromToWarehouseGROUND[math.random(2, table.getn(FromToWarehouseGROUND))]
-    local currFromWarehouse = warehousePair[1]
-    local currToWarehouse = warehousePair[2]
-    local currGroup = GroundTank[math.random(2, table.getn(GroundTank))]
-    local currNumber = math.random(1,5)
-    RequestResourceSelfProp(currFromWarehouse, currToWarehouse, currGroup, currNumber, WAREHOUSE.TransportType.AIRPLANE )
-end
+-- GroundTank = {"TEMPL-RedTank", "TEMPL-RedAAA","TEMPL-RedTruck"}
 
 function ScheduleGROUNDToEast()
-    local warehousePair = FromToGroundPropelled[math.random(2, table.getn(FromToGroundPropelled))]
+    local warehousePair = FromToGroundPropelled[math.random(#FromToGroundPropelled)]
     local currFromWarehouse = warehousePair[1]
     local currToWarehouse = warehousePair[2]
-    local currGroup = GroundTank[math.random(2, table.getn(GroundTank))]
-    local currNumber = math.random(1,5)
+    local currGroup = GroundTank[math.random(#GroundTank)]
+    local currNumber = math.random(5)
     RequestResourceSelfProp(currFromWarehouse, currToWarehouse, currGroup, currNumber, WAREHOUSE.TransportType.SELFPROPELLED)
 end
 
+-- ----------------------------------------------------------------------------------------------------------------
+
 RedCapScheduler = SCHEDULER:New(nil, ScheduleCAPRequest, {}, 10, 20*60, 1.0, 3600*24)
-HeliScheduler = SCHEDULER:New( nil, ScheduleWHrefill, {}, 60, 20*60, 1.0, 3600*24)
+HeliScheduler = SCHEDULER:New(nil, ScheduleHelirefill, {}, 60, 20*60, 1.0, 3600*24)
 GroundUnitScheduler = SCHEDULER:New( nil, ScheduleGROUNDWHRefill, {}, 30, 60*15, 1.0, 3600*24)
-GroundUnitToEastSched = SCHEDULER:New( nil, ScheduleGROUNDToEast, {}, 45, 30*60, 1.0, 3600*24)
+GroundUnitToEastSched = SCHEDULER:New( nil, ScheduleGROUNDToEast, {}, 7200, 30*60, 1.0, 3600*24)
+
+
+function WAREHOUSE:OnAfterAirbaseCaptured(From, Event, To, Coalition)
+    US_CC:MessageTypeToCoalition(string.format("%s , %s, %s, %s",From, Event, To, Coalition, MESSAGE.Type.Information))
+    RU_CC:MessageTypeToCoalition(string.format("%s , %s, %s, %s",From, Event, To, Coalition, MESSAGE.Type.Information))
+end
+
+WAREHOUSE:SetDebugOn()
